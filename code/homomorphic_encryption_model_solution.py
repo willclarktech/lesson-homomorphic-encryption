3dae45a06ac7040aa494254484b949de9f3d145c
willclarktech <willclarktech@users.noreply.github.com>
2021-02-18 18:43:27 +0000

    Rename solution files

--- code/homomorphic_encryption_model_solution.py
+++ code/homomorphic_encryption_model_solution.py
@@ -1,4 +1,5 @@
 from typing import List, Tuple
+
 from paillier import add, encrypt, multiply, PublicKey
 
 
@@ -7,7 +8,12 @@ def encrypted_celsius_to_fahrenheit(public_key: PublicKey, ciphertext: int) -> i
     Returns an encrypted integer representing 1/10ths of a degree Fahrenheit
     °F = °C * 1.8 + 32
     """
-    pass
+    # First multiply the ciphertext by 18.
+    multiplied_by_18 = multiply(public_key, ciphertext, 18)
+    # Now encrypt 320.
+    encrypted_320 = encrypt(public_key, 320)
+    # Finally add them together. The result will need to be divided by 10.
+    return add(public_key, multiplied_by_18, encrypted_320)
 
 
 def encrypted_price_calculator(
@@ -18,4 +24,12 @@ def encrypted_price_calculator(
     """
     Returns the encrypted sum of multiplying each encrypted price by an unencrypted quantity
     """
-    pass
+    item_subtotals = [
+        multiply(public_key, price, quantity) for price, quantity in encrypted_cart
+    ]
+
+    total = encrypt(public_key, 0)
+    for item_subtotal in item_subtotals:
+        total = add(public_key, total, item_subtotal)
+
+    return total
